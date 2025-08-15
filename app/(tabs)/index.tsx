e9ee0fd5217f3d339318ea40eeaa7c395d277f86
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-08-15 13:10:14 +0530

    try

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <ScrollView className="flex-1 px-5"
         showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%", paddingBottom: 10 }}
       >
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        <Image source={icons.logo} className="w-13 h-10 mt-20 mb-5 mx-auto"/>
         
         <View className="flex-1 mt-5">
           <SearchBar
