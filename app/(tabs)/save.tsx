0b1ac172e21c760f90d1e39e77c993e01e560825
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-08-17 23:31:07 +0530

    movie details pade modified, profile and saved pages added

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image} from 'react-native'
+import { icons } from '@/constants/icons'
 import React from 'react'
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex flex-1 items-center justify-center'>
+        <Image className='size-10' tintColor='#fff' source={icons.save}/>
+        <Text className='text-gray-500 text-base'>Save</Text>
+      </View>
     </View>
   )
 }
